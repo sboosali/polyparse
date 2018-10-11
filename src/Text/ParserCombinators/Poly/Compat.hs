936e49b7051d29a4eb924e77b30ddb924b60fee3
Spiros Boosalis <SamBoosalis@gmail.com>
2018-10-10 18:48:51 -0700

    add `.Compat` to conditionally rexport MonadFail

---
+++ src/Text/ParserCombinators/Poly/Compat.hs
@@ -0,0 +1,46 @@
+--------------------------------------------------
+
+{-# LANGUAGE CPP #-}
+
+--------------------------------------------------
+
+#ifndef MIN_VERSION_GLASGOW_HASKELL
+#define MIN_VERSION_GLASGOW_HASKELL(x,y,z1,z2) 0
+#endif
+-- NOTE `ghc-7.10` introduced `MIN_VERSION_GLASGOW_HASKELL`.
+
+--------------------------------------------------
+
+#ifndef MIN_VERSION_base
+#define MIN_VERSION_base(x,y,z) 0
+#endif
+
+--------------------------------------------------
+
+-- | Compatibility module.
+--
+-- Exports 'Control.Monad' and (when present) 'Control.Monad.Fail'.
+-- 
+-- NOTE: `ghc-8.0.*` bundles `base-4.9.0.0`, which introduces `Control.Monad.Fail`.
+--
+
+module Text.ParserCombinators.Poly.Compat
+
+#if MIN_VERSION_base(4,9,0)
+  ( module Control.Monad
+  , module Control.Monad.Fail
+#else
+  ( module Control.Monad
+#endif
+  ) where
+
+--------------------------------------------------
+
+#if MIN_VERSION_base(4,9,0)
+import Control.Monad.Fail (MonadFail(..))
+import Control.Monad      hiding (fail)
+#else
+import Control.Monad
+#endif
+
+--------------------------------------------------
